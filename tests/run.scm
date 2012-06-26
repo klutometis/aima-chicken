a8f1eada848c3e23bf2c0f28e04ddcc4a2dd40e2
Peter Danenberg <pcd@roxygen.org>
2012-06-25 17:21:29 -0700

    New vacuum test

--- tests/run.scm
+++ tests/run.scm
@@ -4,6 +4,12 @@
      test
      vector-lib)
 
+(define clean #t)
+(define clean? identity)
+
+(define dirty #f)
+(define dirty? (complement clean?))
+
 (define (make-vacuum-environment world agent)
   (make-environment
     (lambda ()
@@ -18,29 +24,48 @@
           ((right) (agent-location-set!
                     agent
                     (min (+ (agent-location agent) 1) 1)))
-          ((suck) (vector-set! world (agent-location agent) #f))
+          ((suck) (vector-set! world (agent-location agent) clean))
           (else (error (string-append
                         "make-vacuum-environment:environment-step -- "
                         "Unknown action")
-                       action)))))
-    (lambda ()
-      (vector-count (lambda (i x) (not x)) world))))
+                       action)))))))
 
 (define (make-vacuum-agent location)
   (make-agent
    location
-   (lambda (location dirty?)
-     (if dirty?
-         'suck
+   (lambda (location clean?)
+     (if clean?
          (if (zero? location)
              'right
-             'left)))))
+             'left)
+         'suck))
+   0))
+
+(define (make-vacuum-performance-measure vacuum-world vacuum-agent)
+  (lambda ()
+    (let ((clean-squares
+           (vector-count (lambda (i x) x) vacuum-world)))
+      (agent-score-set! vacuum-agent
+                        (+ (agent-score vacuum-agent)
+                           clean-squares)))))
+
+(define make-vacuum-world vector)
 
 (test
- 2
- (simulate
-  (compose-environments
-   (make-step-limited-environment 1000)
-   (make-vacuum-environment
-    '#(#t #t)
-    (make-vacuum-agent 0)))))
+ "Vacuum agent in clean-clean world"
+ 2000
+ (let ((vacuum-world (make-vacuum-world clean clean))
+       (vacuum-agent (make-vacuum-agent 0)))
+   (let ((vacuum-environment
+          (make-vacuum-environment
+           vacuum-world
+           vacuum-agent)))
+     (simulate
+      (compose-environments
+       (make-step-limited-environment 1000)
+       (make-performance-measuring-environment
+        (make-vacuum-performance-measure
+         vacuum-world
+         vacuum-agent))
+       vacuum-environment))
+     (agent-score vacuum-agent))))
