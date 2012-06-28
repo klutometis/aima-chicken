f9e47b4cb9f7a61559a0a5791b1e3afe0aa9ce7b
Peter Danenberg <pcd@roxygen.org>
2012-06-28 14:41:44 -0700

    Test the penalizing vacuum, too.

--- tests/run.scm
+++ tests/run.scm
@@ -2,6 +2,14 @@
      test)
 
 (test
+ "Non-penalizing vacuum"
  1999
  (simulate-vacuum (make-world dirty clean)
                   (make-reflex-agent left)))
+
+(test
+ "Penalizing vacuum"
+ 1000
+ (simulate-penalizing-vacuum
+  (make-world dirty clean)
+  (make-reflex-agent left)))
