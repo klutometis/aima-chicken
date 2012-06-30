5fa94a419462f115ee881a4cc6b69cd39e6d79ba
Peter Danenberg <pcd@roxygen.org>
2012-06-29 17:35:47 -0700

    Test the stateful reflex agent.

--- tests/run.scm
+++ tests/run.scm
@@ -7,12 +7,20 @@
 (test
  "Non-penalizing vacuum"
  1999
- (simulate-vacuum (make-world dirty clean)
-                  (make-reflex-agent left)))
+ (simulate-vacuum
+  (make-world dirty clean)
+  (make-simple-reflex-agent left)))
 
 (test
  "Penalizing vacuum"
  1000
  (simulate-penalizing-vacuum
   (make-world dirty clean)
-  (make-reflex-agent left)))
+  (make-simple-reflex-agent left)))
+
+(test
+ "Penalizing vacuum with stateful agent"
+ 1995
+ (simulate-penalizing-vacuum
+  (make-world dirty dirty)
+  (make-stateful-reflex-agent left)))
