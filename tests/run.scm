ab27c87418b40a878a811f2e69b25988b38b5a85
Peter Danenberg <pcd@roxygen.org>
2012-07-16 16:52:13 -0700

    Tests for graph world

--- tests/run.scm
+++ tests/run.scm
@@ -24,3 +24,52 @@
  (simulate-penalizing-vacuum
   (make-world dirty dirty)
   (make-stateful-reflex-agent left)))
+
+(parameterize ((current-test-epsilon 0.005))
+  (test
+   "Randomized graph agent on 100 different worlds"
+   17300.0
+   (let* ((scores
+           (list-tabulate
+            100
+            (lambda (i)
+              (let* ((world (make-graph-world))
+                     (start (random-start world))
+                     (agent (make-randomized-graph-agent start)))
+                (parameterize ((random-seed i)
+                               (debug? #f))
+                  (simulate-graph world agent))
+                (agent-score agent))))))
+     (/ (apply + scores) (length scores))))
+  
+  (test
+   "Stateful graph agent on 100 different worlds"
+   19176.35
+   (let* ((scores
+           (list-tabulate
+            100
+            (lambda (i)
+              (let* ((world (make-graph-world))
+                     (start (random-start world))
+                     (agent (make-stateful-graph-agent start)))
+                (parameterize ((random-seed i)
+                               (debug? #f))
+                  (simulate-graph world agent))
+                (agent-score agent))))))
+     (/ (apply + scores) (length scores)))))
+
+(parameterize ((current-test-epsilon 0.1))
+  (test
+   "Randomized graph agent on a linear world 100 times"
+   15000.0
+   (let* ((world (make-linear-world))
+          (start (random-start world)))
+     (let ((scores (list-tabulate
+                    100
+                    (lambda (i)
+                      (let* ((world (copy-world world))
+                             (agent (make-randomized-graph-agent start)))
+                        (parameterize ((debug? #f))
+                          (simulate-graph world agent))
+                        (agent-score agent))))))
+       (/ (apply + scores) (length scores))))))
